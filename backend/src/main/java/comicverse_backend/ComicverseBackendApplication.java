5913d1064b1d586a0802d44da5e77b720cf259c7
Tejas More <tnmore2001@gmail.com>
2026-06-12 23:28:38 +0530

    Initial Spring Book backend setup

---
+++ backend/src/main/java/comicverse_backend/ComicverseBackendApplication.java
@@ -0,0 +1,13 @@
+package comicverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicverseBackendApplication.class, args);
+	}
+
+}
